dccb8902120fb2ebbf70f3a8209d1ea60fd10687
mahdi chekini <me.chekini@gmail.com>
2021-02-13 23:57:25 +0100

    configuring logs for product

--- order/src/main/java/com/illishop/order/controller/OrderController.java
+++ order/src/main/java/com/illishop/order/controller/OrderController.java
@@ -2,13 +2,17 @@ package com.illishop.order.controller;
 
 import com.illishop.order.model.Order;
 import com.illishop.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class OrderController {
+
 
     private final OrderService service;
 
@@ -24,5 +28,15 @@ public class OrderController {
     @GetMapping("/orders")
     public List<Order> getAllOrders() {
         return service.getAllOrders();
+    }
+
+
+    @PostConstruct
+    public void generateError() {
+        try {
+            int a = 5 / 0;
+        } catch (Exception e) {
+            log.error("division par 0 interdite selon le maestro du Milan AC", e);
+        }
     }
 }
